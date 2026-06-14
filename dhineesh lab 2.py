316cfd5369599b31803c5bbb77961913b9b56558
dhineesh2008 <dhineeshmuthusamy179@gmail.com>
2026-06-14 21:39:57 +0530

    Add files via upload

---
+++ dhineesh lab 2.py
@@ -0,0 +1,174 @@
+'''#even or odd
+
+a = int(input("Enter a: "))
+
+if a % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+#eligible or not
+
+a = int(input("Enter a: "))
+if a >= 18:
+    print("eligible")
+else:
+    print("not eligible")
+
+#passes or fails
+
+a = int(input("Enter a:"))
+if a >= 40:
+    print("pass")
+else:
+    print("fail")
+
+#positive or negative
+
+a = int (input("Enter a:"))
+if a > 0:
+   print("positive")
+else:
+    print("negtive")
+
+#elif
+#grade based mark
+
+a = int(input(" Enter a:"))
+if a >= 90:
+    print ("well done")
+elif a >= 85:
+    print ("very good")
+elif a >= 50:
+    print ("should improve")
+else: 
+    print ("fail")
+
+#largest three number
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+if a <= b:
+  print ("b is largest number")
+elif b <= c:
+  print ("c is largest number")
+else:
+  print ("a is largest number")
+
+#calculator (+,-,*,/)
+#add
+
+a = int(input("Enter a: "))
+if a+4==0 or a+9==0:
+    print("done")
+else:
+    print("not done")
+
+#subraction
+
+a = int(input("Enter a:"))
+if a-5==0 or a-9==0:
+    print("done")
+else:
+    print("not done")
+    
+#multiplication
+
+a = int(input("Enter a:"))
+if a*4==0 or a*9==0:
+    print("done")
+else:
+    print("not done")
+
+#division
+
+a = int(input("Enter a:"))
+if a/4==0 or a/9==0:
+    print("done")
+else:
+    print("not done")
+
+#username password arew correct
+
+u = int(input("Enter a username:"))
+p = int(input("Enter a password:"))
+
+if u ==raghav44:
+       print("Enter password")
+         if p == 34768509:
+            print("loging success")
+         else:  
+            print("invalide password")
+else:
+       print("invalide username")
+
+#traffic signal system
+
+print(" Red -> stop ")
+print(" yellow -> get ready ")
+print("green -> go ")
+
+r =  int(input("Enter red:"))
+if r==0:
+         print("stop")
+         y = int(input("Enter yellow:"))
+         if y == 0:
+             print("get ready")
+             g = int(input("Enter green:"))
+             if g == 0:
+                 print("go")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
