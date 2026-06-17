49c9fa7070fa5a9307764970c5433d0aad8611fe
dhineesh2008 <dhineeshmuthusamy179@gmail.com>
2026-06-17 17:04:40 +0530

    Add files via upload

--- dhineesh lab 5.py
+++ dhineesh lab 5.py
@@ -1,99 +1,255 @@
-#1.
-a="dhineesh"
-print(a.upper())
+#break statement(for loop)
+ #1
+for i in range(1,20):
+    if i==10:
+        break
+print(i)
 
-#2.
-b="you can do it"
-print(b.lower())
-
-#3.
-c="title"
-print(c.capitalize())
-
-#4.
-name="dhineesh"
-age=18
-print("my name is {}and I am {} years old.".format(name,age))
-
-#5.
-d="try your best"
-print(d.index('o'))
-print(d.index('y'))
-print(d.index('t'))
-
-#6.
-
-sentence="python is easy to learn"
-substring="1"
-
-position=sentence.find(substring)
-
-print("position:",position)
-
-#7.
-
-e=input("Enter a word:")
-
-if e.endswith("ing"):
-    print("the word end with 'ing'.")
+#2
+correct_pass="dhin123456"
+a=str(input("Enter a:"))
+while true:
+   if a==correct_pass:
+     print("ok!!")
+     break
 else:
-    print("the word does not end with 'ing'.")
+    print("not ok!!")
 
-#8.
-
-text = "name\tof\tthe company"
-print("Original string:")
-print(text)
-print("After expandtabs():")
-print(text.expandtabs())
-
-#9.
-e="dhineesh!"
-f=e.encode("utf-8")
-print(f)
-
-#10.
-g=input("Enter a string:")
-
-if g.isdigit():
-    print("the string contain only digits.")
+#3
+a=int(input("Enter a"))
+if i==infinity:
+    print("ok!!")
+    break
 else:
-    print("the string does not contain only digitals.")
+    print("not ok!!!")
 
-#11.
-h=input("Enter a string:")
+#4
 
-if h.isnumeric():
-    print("the string is numeric.")
-else:
-    print("the string is not numeric.")
+for i in range(1,50):
+    if i==10:
+        break
+    print(i)
 
-#12.
-i=input("Enter a string:")
+#5
 
-if i.isalnum():
-    print("the string is isalnum.")
+for i in range(1,100):
+    if i==34:
+        break
+    print(i)
+
+#6
+e=int(input("Enter a:"))
+if i==Exit:
+    print("done!!")
+      break
 
 else:
-    print("the string is not isalnum.")
+    print("not done")
 
-#13.
-
-j=input("Enter a string:")
-
-if j.isascii():
-    print("the string contains only ASCII characters.")
+#7
+c=int(input("Enter a:"))
+if i== quit:
+    print("Done")
+       break
 else:
-    print("the string contains non-ASCII characters.")
+    print("not DONE")
 
-#14.
-
-k= input("Enter a string:")
-
-if k.isalpha():
-    print("the string is isalpha.")
+#8
+for i in range(1,100):
+    if i==2:
+        break
+        print("ok!!")
+           
 else:
-    print("the string is not isalpha.")
+    print("not ok!!")
+
+
+#continue statement
+#1
+for i in range(1,20):
+    if i==10:
+        continue
+    print(i)
+
+#2
+
+for i in range(1,50):
+    if i%2==0:
+      continue
+    print(i)
+
+#3
+
+for i in range(1,50):
+    if i%2==0:
+        continue
+    print(i)
+
+#4
+for i in range(1,100):
+    if i%5==0:
+        continue
+    print(i)
+
+#5
+
+name = str(input("Enter name:"))
+for ch in name:kavinkumar
+if ch in "DHINEEdhinee":
+     continue
+    print(ch):
+
+#6
+for i in range(1,30):
+    if i%2==0:
+        continue
+    print(i)
+
+#7
+a=int(input("Enter a:"))
+if i-0==0:
+     continue
+print(i)
+
+#8
+for i in range(1,20):
+    if i%2==0:
+        continue
+    print(i)
+
+#pass statement
+
+#1`
+
+creat_empty_function="student_detail"
+a=str(input("Enter a:"))
+while True:
+    if a==creat_empty_function:
+        print("ok!!")
+else:
+    print("not ok!!")
+
+
+#2
+create_empty_class="employee"
+a=str(input("Enter:"))
+while True:
+    if a==create_empty_class:
+        print("ok")
+else:
+    print("not ok")
+
+#3
+
+i=1
+while i<=5:
+    break
+print(i)
+i+=1
+
+#4
+for i in range(20):
+    break
+print(i)
+
+#5
+create_placeholder="bankaccount"
+a=str(input("Enter a:"))
+while True:
+    if a==bankAccount:
+        print("ok")
+    else:
+        print("not ok")
+#while loop
+#break
+#1
+i=10
+while i<=10:
+    break
+    print(i)
+    i+=1
+
+#2
+
+i=20
+while i<=20:
+    break
+print(i)
+i+=1
+
+#3
+
+i=100
+while i<=100:
+    if i==50:
+     break
+print(i)
+i+=1
+
+#continue
+#1
+i=10
+while i>1
+    if i==5:
+       continue
+    print(i)
+
+#2
+i=20
+while i>1:
+ if  i==10:
+     continue
+ print(i)
+ i-=1
+
+#3
+i=20
+while i>1:
+    if i%2==0:
+        continue
+     print(i)
+     i-=1
+
+#pass
+#1
+i=1
+while i<6:
+    if i==4:
+       break
+
+#2
+
+i=1
+while i<5:
+    if i==3:
+      break
+    print(i)
+    i+=1
+
+#3
+i=1
+while i<1:
+      if i==5:
+         break
+       print(i)
+       i-=1
+
+#mixed question
+#1
+for i in range(1,20):
+    if i==15:
+       break
+    print(i)
+for i in range(1,20):
+    if i==5:
+        continue
+     print(i)
+
+#2
+for i in range(1,50,1):
+    if i%2==0:
+       break
+    print(i). 
 
 
 
@@ -102,4 +258,47 @@ else:
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
